64a568f00f69b984c5ca952f6f84357f236671b5
naveen <knaveen@cykul.com>
2019-01-11 17:25:08 +0530

    Initial Commit

---
+++ CoreDataProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by MAC BOOK on 11/01/19.
+//  Copyright © 2019 MAC BOOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
